662f84aaf7acf895a1e0d14fc7294b439606ebc9
sseon000 <rlatjsgur000@naver.com>
2024-06-10 22:05:29 +0900

    [ setting ] - securityconfig init

--- src/main/java/com/fsje/dairy/DairyApplication.java
+++ src/main/java/com/fsje/dairy/DairyApplication.java
@@ -2,7 +2,6 @@ package com.fsje.dairy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import com.fsje.dairy.common.config.DataBaseConfig;
--- src/main/java/com/fsje/dairy/common/config/SecurityConfig.java
+++ src/main/java/com/fsje/dairy/common/config/SecurityConfig.java
@@ -1,16 +1,72 @@
 package com.fsje.dairy.common.config;
 
-import java.util.List;
+import java.util.Arrays;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-/*
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 @Configuration
-@ConfigurationProperties(prefix = "spring.security")
-*/
+@EnableWebSecurity
 public class SecurityConfig {
-	/*
-	private List<String> sataticPatterns;
-	private List<String> authExcludePatterns;
-	*/
+	
+	//Cors 설정
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Arrays.asList("*"));
+	    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+	    configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Content-Type", "Authorization", "X-XSRF-token"));
+	    configuration.setAllowCredentials(false);
+	    configuration.setMaxAge(3600L);
+
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
+	}
+
+	//
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .formLogin(Customizer.withDefaults())
+            .formLogin(login -> login
+                    .loginPage("/login")
+                    .successHandler(new SimpleUrlAuthenticationSuccessHandler("/"))
+                    .permitAll()
+            )
+            .authorizeHttpRequests(authorizeRequest -> authorizeRequest
+                            .requestMatchers(AntPathRequestMatcher.antMatcher("/")).permitAll()
+                            .requestMatchers(AntPathRequestMatcher.antMatcher("/user")).permitAll()
+                            .anyRequest().authenticated()
+            )
+            .headers(headersConfigurer -> headersConfigurer
+            	.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // 정적 리소스 spring security 대상에서 제외
+        return (web) -> web
+                    .ignoring()
+                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 }
