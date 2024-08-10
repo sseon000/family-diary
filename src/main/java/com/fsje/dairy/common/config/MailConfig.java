5d093ecadff9486a86749cfebf9512067bbd1b22
sseon000 <rlatjsgur000@naver.com>
2024-08-10 22:16:58 +0900

    [ feature_12_02 ][gmail smtp]
    - 설정 수정

--- src/main/java/com/fsje/dairy/common/config/MailConfig.java
+++ src/main/java/com/fsje/dairy/common/config/MailConfig.java
@@ -10,31 +10,39 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import com.fsje.dairy.common.properties.MailProperties;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class MailConfig {
-	    Properties pt = new Properties();
-	    private final MailProperties mailProperties;
+    Properties pt = new Properties();
+    private final MailProperties mailProperties;
 
-	    @Bean
-	    public JavaMailSender javaMailService() {
-	        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-	        javaMailSender.setHost(mailProperties.getHost());
-	        javaMailSender.setUsername(mailProperties.getUserName());
-	        javaMailSender.setPassword(mailProperties.getPassword());
-	        javaMailSender.setPort(mailProperties.getPort());
+    @Bean
+    public JavaMailSender javaMailService() {
+    	log.info("host : " + mailProperties.getHost());
+    	log.info("userName : " + mailProperties.getUserName());
+    	log.info("pw : " + mailProperties.getPassword());
+    	log.info("port : " + mailProperties.getPort());
+    	
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(mailProperties.getHost());
+        javaMailSender.setUsername(mailProperties.getUserName());
+        javaMailSender.setPassword(mailProperties.getPassword());
+        javaMailSender.setPort(mailProperties.getPort());
 
-	        pt.put("mail.smtp.socketFactory.port", mailProperties.getPort());
-	        pt.put("mail.smtp.auth", true);
-	        pt.put("mail.smtp.starttls.enable", true);
-	        pt.put("mail.smtp.starttls.required", true);
-	        pt.put("mail.smtp.socketFactory.fallback", false);
-	        //pt.put("mail.smtp.socketFactory.class", mailProperties.getSocketFactoryClass());
+        pt.put("mail.smtp.socketFactory.port", mailProperties.getPort());
+        pt.put("mail.smtp.auth", true);
+        pt.put("mail.smtp.starttls.enable", true);
+        pt.put("mail.smtp.starttls.required", true);
+        pt.put("mail.smtp.socketFactory.fallback", false);
+        //pt.put("mail.smtp.socketFactory.class", mailProperties.getSocketFactoryClass());
 
-	        javaMailSender.setJavaMailProperties(pt);
-	        javaMailSender.setDefaultEncoding("UTF-8");
+        javaMailSender.setJavaMailProperties(pt);
+        javaMailSender.setDefaultEncoding("UTF-8");
 
-	        return javaMailSender;
-	    }
-	}
+        return javaMailSender;
+    }
+}
--- src/main/java/com/fsje/dairy/common/properties/MailProperties.java
+++ src/main/java/com/fsje/dairy/common/properties/MailProperties.java
@@ -1,24 +1,19 @@
 package com.fsje.dairy.common.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
-@ConfigurationProperties(prefix="mail")
-@ConfigurationPropertiesScan
-@RequiredArgsConstructor
 @Data
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix="mail")
 public class MailProperties {
-	// SMTP 서버
-    private final String host;
-    // 포트번호
-    private final int port;
-    // 계정
-    private final String userName;
-    // 비밀번호
-    private final String password;
-    // 인코딩
-    private final String defaultEncoding;
+    private final String host;            //SMTP 서버
+    private final int    port;            //포트번호
+    private final String userName;        //계정
+    private final String password;        //비밀번호
+    private final String defaultEncoding; //인코딩
 }
